ccaafd9a62e6f0a41ada540555d4d2e4242d0874
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-10-11 03:49:37 +0600

    question page added

--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -8,7 +8,7 @@ const Questions = () => {
     return (
         <div>
             <h1 className='font-bold text-3xl my-7 text-center'>Select The Right Answer</h1>
-            <div className='grid grid-cols-1 gap-4'>
+            <div className='grid grid-cols-1 gap-4 w-4/5 mx-auto'>
                 {
                     questions.map(question => <div key={question.id} className='card w-full bg-base-100 shadow-xl'>
                         <div className="card-body">
@@ -16,26 +16,26 @@ const Questions = () => {
                             <div className="card-actions">
                                 <div className="form-control">
                                     <label className="label cursor-pointer">
-                                        <input type="radio" name="radio-6" className="radio checked:bg-red-500" />
-                                        <span className="label-text font-bold text-lg">{question.options[0]}</span>
+                                        <input type="radio" name={question.id} className="radio checked:bg-red-500" />
+                                        <span className="label-text font-bold text-lg"> {question.options[0]}</span>
                                     </label>
                                 </div>
                                 <div className="form-control">
                                     <label className="label cursor-pointer">
-                                        <input type="radio" name="radio-6" className="radio checked:bg-red-500" />
-                                        <span className="label-text font-bold text-lg">{question.options[1]}</span>
+                                        <input type="radio" name={question.id} className="radio checked:bg-red-500" />
+                                        <span className="label-text font-bold text-lg"> {question.options[1]}</span>
                                     </label>
                                 </div>
                                 <div className="form-control">
                                     <label className="label cursor-pointer">
-                                        <input type="radio" name="radio-6" className="radio checked:bg-red-500" />
-                                        <span className="label-text font-bold text-lg">{question.options[2]}</span>
+                                        <input type="radio" name={question.id} className="radio checked:bg-red-500" />
+                                        <span className="label-text font-bold text-lg"> {question.options[2]}</span>
                                     </label>
                                 </div>
                                 <div className="form-control">
                                     <label className="label cursor-pointer">
-                                        <input type="radio" name="radio-6" className="radio checked:bg-red-500" />
-                                        <span className="label-text font-bold text-lg">{question.options[3]}</span>
+                                        <input type="radio" name={question.id} className="radio checked:bg-red-500" />
+                                        <span className="label-text font-bold text-lg"> {question.options[3]}</span>
                                     </label>
                                 </div>
                             </div>
